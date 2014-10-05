11a34daf316aa010a65b0b5e9f8dc84553760ced
vpurush <vijayaraghavan1987@gmail.com>
2014-10-05 15:21:24 +0530

    Bootstrap and jquery files added, server code changes done to serve them

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,8 @@
 ﻿var express = require('express'),
     app = express(),
-    authenticator = require('./Authentication/Authentication');
+    authenticator = require('./Authentication/Authentication'),
+    path = require('path'),
+    fs = require('fs');
 
 authenticator.Configure(app);
 app.listen(81);
@@ -10,5 +12,39 @@ app.get('/', authenticator.Authenticate, function (req, res) {
 });
 
 app.get('/login-signup', function (req, res) {
-    res.send('This is login page');
+    res.status(200).sendFile(path.join(__dirname, '../UI/HTML/LoginSignup.html'));
+});
+
+var sendFileIfExists = function (filePath, res) {
+    fs.exists(filePath, function (exists) {
+        if (exists) {
+            res.status(200).sendFile(filePath);
+        } else {
+            res.status(404).end();
+        }
+    });
+};
+
+app.get('/CSS/:name', function (req, res) {
+    var fileName = req.param('name');
+    var filePath = path.join(__dirname, '../UI/CSS/', fileName);
+    sendFileIfExists(filePath, res);
+});
+
+app.get('/Scripts/Thirdparty/:name', function (req, res) {
+    var fileName = req.param('name');
+    var filePath = path.join(__dirname, '../UI/Scripts/Thirdparty/', fileName);
+    sendFileIfExists(filePath, res);
+});
+
+app.get('/Scripts/:name', function (req, res) {
+    var fileName = req.param('name');
+    var filePath = path.join(__dirname, '../UI/Scripts/', fileName);
+    sendFileIfExists(filePath, res);
+});
+
+app.get('/Fonts/:name', function (req, res) {
+    var fileName = req.param('name');
+    var filePath = path.join(__dirname, '../UI/Fonts/', fileName);
+    sendFileIfExists(filePath, res);
 });
